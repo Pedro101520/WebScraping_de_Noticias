44348482ed4bdb9362a0b786bf9f264666236554
Pedro101520 <pedrolimazoi10@gmail.com>
2024-04-10 17:02:22 -0300

    Atualização

--- Scraping/PDF.py
+++ Scraping/PDF.py
@@ -10,7 +10,7 @@ from reportlab.platypus import SimpleDocTemplate, Paragraph
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 
 def geraPdf(qtde_noticias, siteNoticia):
-    pdf = SimpleDocTemplate("Ultimas_Noticias.pdf", pagesize=letter)
+    # pdf = SimpleDocTemplate("Ultimas_Noticias.pdf", pagesize=letter)
 
     estilo_titulo = ParagraphStyle(
         "titulo",
@@ -45,26 +45,31 @@ def geraPdf(qtde_noticias, siteNoticia):
         titulo_texto = tituloUol()
         datalocal_texto = HoraUol()
         link_texto = linksUol()
+        pdf = SimpleDocTemplate("Noticias_UOL.pdf", pagesize=letter)
     elif(siteNoticia == 'g'):
         titulo_texto = tituloG1()
         datalocal_texto = localHoraG1()
         link_texto = linksG1()
         descricao_texto = descricaoG1()
+        pdf = SimpleDocTemplate("Noticias_G1.pdf", pagesize=letter)
     elif(siteNoticia == 'f'):
         titulo_texto = tituloFolha()
         datalocal_texto = HoraFolha()
         link_texto = linksFolha()
         descricao_texto = descricaoFolha()
+        pdf = SimpleDocTemplate("Noticias_FolhaDeSaoPaulo.pdf", pagesize=letter)
     elif(siteNoticia == 'c'):
         titulo_texto = tituloCNN()
         datalocal_texto = HoraCNN()
         link_texto = linksCNN()
         descricao_texto = descricaoCNN(qtde_noticias)
+        pdf = SimpleDocTemplate("Noticias_CNN.pdf", pagesize=letter)
     elif(siteNoticia == 'j'):
         titulo_texto = tituloJovem()
         datalocal_texto = HoraJovem()
         link_texto = linksJovem()
         descricao_texto = descricaoJovem(qtde_noticias)
+        pdf = SimpleDocTemplate("Noticias_JovemPan.pdf", pagesize=letter)
 
     for i in range(qtde_noticias):
         # Adiciona o título ao conteúdo com quebra de linha
