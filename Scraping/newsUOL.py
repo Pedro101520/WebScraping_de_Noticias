2581819753c741f4ba28cbdb261b00abd4864c78
Pedro101520 <pedrolimazoi10@gmail.com>
2024-10-03 11:47:39 -0300

    Adição do User-Agent, para ser possível o acesso no site do UOL

--- Scraping/newsUOL.py
+++ Scraping/newsUOL.py
@@ -1,29 +1,50 @@
-from urllib.request import urlopen
+import requests
 from bs4 import BeautifulSoup
 
-url = urlopen("https://noticias.uol.com.br/ultimas/")
-parsed_html = BeautifulSoup(url, "html.parser")
+url = "https://noticias.uol.com.br/ultimas/"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive"
+}
 
-def tituloUol():
-    textoTitulo = []
-    titulos = parsed_html.find_all('div', attrs={'class': 'thumb-caption'})
-    for titulo in titulos:
-        conteudo = titulo.find('h3').get_text()
-        textoTitulo.append(conteudo)
-    return textoTitulo
+try:
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
 
-def HoraUol():
-    armazenaInfo = []
-    infos = parsed_html.find_all('div', attrs={'class': 'thumb-caption'})
-    for info in infos:
-        conteudo = info.find('time').get_text()
-        armazenaInfo.append(conteudo)
-    return armazenaInfo
+    parsed_html = BeautifulSoup(response.text, "html.parser")
 
-def linksUol():
-    armazenaLink = []
-    links = parsed_html.find_all('div', attrs={'class': 'thumbnails-wrapper'})
-    for link in links:
-        url = link.find('a')
-        armazenaLink.append(url.get('href'))
-    return armazenaLink
+    def tituloUol():
+        textoTitulo = []
+        titulos = parsed_html.find_all('div', attrs={'class': 'thumb-caption'})
+        for titulo in titulos:
+            conteudo = titulo.find('h3').get_text()
+            textoTitulo.append(conteudo)
+        return textoTitulo
+
+    def HoraUol():
+        armazenaInfo = []
+        infos = parsed_html.find_all('div', attrs={'class': 'thumb-caption'})
+        for info in infos:
+            conteudo = info.find('time').get_text()
+            armazenaInfo.append(conteudo)
+        return armazenaInfo
+
+    def linksUol():
+        armazenaLink = []
+        links = parsed_html.find_all('div', attrs={'class': 'thumbnails-wrapper'})
+        for link in links:
+            url = link.find('a')
+            armazenaLink.append(url.get('href'))
+        return armazenaLink
+
+    titulos = tituloUol()
+    horas = HoraUol()
+    links = linksUol()
+
+
+except requests.exceptions.HTTPError as e:
+    print(f"Erro ao acessar a página: {e}")
+except Exception as e:
+    print(f"Erro: {e}")
